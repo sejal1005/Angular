1dea9be7a72b139e7fd1c551fd639f11d9cf3ff5
sejal1005 <sejalchoudhary322@gmail.com>
2025-07-08 17:09:19 +0530

    initial commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,33 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, effect, signal} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ProfileComponent } from "./profile/profile.component";
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ProfileComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  
-//   count=0
+//  COUNTER APPLICATION 
+  // count=0
   
 
-//   handleCounter(val:string){
-//     if(val=='minus'){
-//       this.count=this.count-1
-//       if(this.count==-1){
-//         this.count=0
-//       }
-//     }
-//     else if(val=='plus'){
-//       this.count=this.count+1
-//     }
-//     else{
-//       this.count=0
-//     }
-//   }
-
+  // handleCounter(val:string){
+  //   if(val=='minus'){
+  //     this.count=this.count-1
+  //     if(this.count==-1){
+  //       this.count=0
+  //     }
+  //   }
+  //   else if(val=='plus'){
+  //     this.count=this.count+1
+  //   }
+  //   else{
+  //     this.count=0
+  //   }
+  // }
+// EVENT HANDLING
 //   handleEvent(event:MouseEvent){
 //     console.log("function called",event.type);
 //      console.log("function called",(event.target as Element).className);
@@ -38,25 +38,49 @@ export class App {
 //   const input = event.target as HTMLInputElement;
 //   console.log(`Event type: ${event.type}, Value: ${input.value}`);
 // }
-name=""
-displayName="";
-email="";
-getName(event:Event){
-  const name=(event.target as HTMLInputElement).value
-  this.name=name;
-}
-showName(){
-  this.displayName=this.name;
-}
-setName(){
-  this.name="Tanishq";
-}
-getEmail(val:string){
-  console.log(val);
-  this.email=val
-}
-setEmail(val:string){
-  console.log(val);
-  this.email="default@test.com"
-}
+// GETTER AND SETTER
+// name=""
+// displayName="";
+// email="";
+// getName(event:Event){
+//   const name=(event.target as HTMLInputElement).value
+//   this.name=name;
+// }
+// showName(){
+//   this.displayName=this.name;
+// }
+// setName(){
+//   this.name="Tanishq";
+// }
+// getEmail(val:string){
+//   console.log(val);
+//   this.email=val
+// }
+// setEmail(val:string){
+//   console.log(val);
+//   this.email="default@test.com"
+// }
+// SIGNAL
+// count = signal(10);
+// x=20;
+
+// constructor(){
+//   effect(()=>{
+//     console.log(this.count());
+//   })
+// }
+// updateValue(val:string){
+  // this.count.set(this.count()+1);
+
+  // if(val=='inc'){
+  //   this.count.set(this.count()+1);
+  // }
+  //  if(val=='dec'){
+  //   this.count.set(this.count()-1);
+  // }
+// }
+
+
+ 
+
 }
--- src/app/profile/profile.component.ts
+++
@@ -1,7 +0,0 @@
-import { Component } from "@angular/core";
-
-@Component({
-    selector: 'app-profile',
-    template: '<h1>Profile Component </h1>'
-})
-export class ProfileComponent{}
